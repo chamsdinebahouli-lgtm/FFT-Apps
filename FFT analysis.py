c535d411633ee0c14f1473466167ed14a809f8dd
chamsdinebahouli-lgtm <chamsdine.bahouli@gmail.com>
2025-08-23 01:53:50 +0200

    Update FFT analysis.py

--- FFT analysis.py
+++ FFT analysis.py
@@ -257,9 +257,9 @@ if uploaded_file1 is not None and uploaded_file2 is not None:
                  mag_fundamental2 = magnitude_pos2[fundamental_index2]
 
             if mag_fundamental1 < mag_fundamental2:
-                comparison_result = "Signal 1 est potentiellement meilleur (amplitude fondamentale plus élevée)."
+                comparison_result = "Signal 1 est potentiellement meilleur (amplitude fondamentale plus basse)."
             elif mag_fundamental2 < mag_fundamental1:
-                comparison_result = "Signal 2 est potentiellement meilleur (amplitude fondamentale plus élevée)."
+                comparison_result = "Signal 2 est potentiellement meilleur (amplitude fondamentale plus basse)."
             else:
                 # If fundamental frequencies are similar, compare based on noise power
                 if noise_power1 < noise_power2:
